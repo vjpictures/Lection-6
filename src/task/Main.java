625315151b90897d75c170c368c908f2eac83915
vjpictures <vjpictures@mail.ru>
2024-02-01 20:04:28 +0100

    corrections

--- src/task/Main.java
+++ src/task/Main.java
@@ -1,66 +1,39 @@
 package task;
-import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
         Phone phone1 = new Phone();
+        phone1.setNumber(555444733);
+        phone1.setModel("Samsung");
+        phone1.setWeight(50);
+        phone1.setName("Adam");
         Phone phone2 = new Phone();
+        phone2.setNumber(775444253);
+        phone2.setModel("Motorola");
+        phone2.setWeight(50);
+        phone2.setName("Max");
         Phone phone3 = new Phone();
+        phone3.setNumber(45647784);
+        phone3.setModel("Iphone");
+        phone3.setWeight(50);
+        phone3.setName("Alex");
         Phone phone = new Phone();
 
-        System.out.print("Введите номер 1-го телефона: ");
-        phone1.number = getNum();
-        System.out.print("Введите его модель: ");
-        phone1.model = getStr();
-        System.out.print("Введите вес телефона: ");
-        phone1.weight = getNum();
-        System.out.print("Введите имя владельца: ");
-        phone1.name = getStr();
-        System.out.print("Введите номер 2-го телефона: ");
-        phone2.number = getNum();
-        System.out.print("Введите его модель: ");
-        phone2.model = getStr();
-        System.out.print("Введите вес телефона: ");
-        phone2.weight = getNum();
-        System.out.print("Введите имя владельца: ");
-        phone2.name = getStr();
-        System.out.print("Введите номер 3-го телефона: ");
-        phone3.number = getNum();
-        System.out.print("Введите его модель: ");
-        phone3.model = getStr();
-        System.out.print("Введите вес телефона: ");
-        phone3.weight = getNum();
-        System.out.print("Введите имя владельца: ");
-        phone3.name = getStr();
-
-
-        System.out.println("\n" + "Phone №1: " + "\n" + "Number: " + phone1.number + "; Model: " + phone1.model + "; Weight: " + phone1.weight);
-        System.out.println("Pnone №2: " + "\n" + "Number: " + phone2.number + "; Model: " + phone2.model + "; Weight: " + phone2.weight);
-        System.out.println("Phone №3: " + "\n" + "Number: " + phone3.number + "; Model: " + phone3.model + "; Weight: " + phone3.weight);
+        System.out.println("\n" + "Phone №1: " + "\n" + "Number: " + phone1.getNumber() + "; Model: " + phone1.getModel() + "; Weight: " + phone1.getWeight());
+        System.out.println("Pnone №2: " + "\n" + "Number: " + phone2.getNumber() + "; Model: " + phone2.getModel() + "; Weight: " + phone2.getWeight());
+        System.out.println("Phone №3: " + "\n" + "Number: " + phone3.getNumber() + "; Model: " + phone3.getModel() + "; Weight: " + phone3.getWeight());
         System.out.println();
         phone1.receiveCall();
-        phone1.getNumber();
         System.out.println();
         phone2.receiveCall();
-        phone2.getNumber();
         System.out.println();
         phone3.receiveCall();
-        phone3.getNumber();
 
-        phone1 = new Phone(phone1.number, phone1.model, phone1.weight, phone1.name);
+        phone1 = new Phone(phone1.getNumber(), phone1.getModel(), phone1.getWeight(), phone1.getName());
         System.out.println();
-        phone.receiveCall(phone1.name, phone1.number);
+        phone.receiveCall(phone1.getName(), phone1.getNumber());
         System.out.println();
-        phone.sendMessage(phone1.number, phone2.number, phone3.number);
+        phone.sendMessage(phone1.getNumber(), phone2.getNumber(), phone3.getNumber());
     }
 
-    static int getNum() {
-        Scanner num = new Scanner(System.in);
-        return num.nextInt();
-    }
-
-    static String getStr() {
-        Scanner str = new Scanner(System.in);
-        return str.nextLine();
-    }
 }
--- src/task/Phone.java
+++ src/task/Phone.java
@@ -1,35 +1,50 @@
 package task;
-
 public class Phone {
-    public int number;
-    public String model;
-    public int weight;
-    public String name;
+    private int number;
+    private String model;
+    private int weight;
+    private String name;
 
+    public Phone() {
+    }
+    public Phone(int number, String model, int weight, String name) {
+        this.number = number;
+        this.model = model;
+        this.weight = weight;
+        this.name = name;
+    }
+    public int getNumber(){
+        return number;
+    }
+    public void setNumber(int number){
+        this.number = number;
+    }
+    public String getModel(){
+        return model;
+    }
+    public void setModel(String model){
+        this.model = model;
+    }
+    public int getWeight(){
+        return weight;
+    }
+    public void setWeight(int weight){
+        this.weight = weight;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
     public void receiveCall() {
         System.out.println("Звонит: " + name);
     }
-
     public void receiveCall(String name, int num) {
         System.out.println("Повторный звонок: " + "\n" + name + "\n" + num);
     }
-
-    public void getNumber() {
-        System.out.println(number);
-    }
-
     public void sendMessage(int number1, int number2, int number3) {
         System.out.println("Сообщение отправлено: " + "\n" + number1 + "\n" + number2 + "\n" + number3);
-    }
-
-    public Phone(int a, String b, int c, String d) {
-        number = a;
-        model = b;
-        weight = c;
-        name = d;
-    }
-
-    public Phone() {
     }
 }
 
